d4e2dac4aeb768020a2fba206d288ed4e425d51f
Kim Nes Leirvik <kim.leirvik@niva.no>
2023-03-14 11:20:55 +0100

    test

--- geonode/api/seabee/metadata.py
+++ geonode/api/seabee/metadata.py
@@ -4,14 +4,26 @@ from geonode.layers.models import Dataset, Style
 from tastypie.authentication import MultiAuthentication, SessionAuthentication
 from ..authentication import OAuthAuthentication
 from geonode.api.authorization import GeonodeApiKeyAuthentication
+#see dataset_upload_metadata
+from geonode.resource.utils import update_resource
 
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
+from geonode.layers.metadata import parse_metadata
 class Metadata(APIView):
     def get(self, request):
-        print(request)
-        return Response({"articles": "hei"})
+       
+        with open("./geonode/api/seabee/sample.xml", "r") as f:
+            xml = f.read()
+        
+        identifier, vals, regions, keywords, custom = parse_metadata(xml)
+        return Response({
+        "identifier":identifier,
+        "vals":vals,
+        "regions":regions,
+        "keywords":keywords,
+        "custom":custom,
+        })
 
 #authentication = MultiAuthentication(SessionAuthentication(),
 #                                             OAuthAuthentication(),
